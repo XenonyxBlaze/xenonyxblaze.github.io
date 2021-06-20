e80af3bf55e969b33551c92ae03c0b8d706286ad
AaravRajput <aarav.rajput2003@gmail.com>
2021-06-20 19:56:10 +0530

    started portfolio

--- js/info.js
+++ js/info.js
@@ -4,26 +4,37 @@ let ageEl = document.getElementById("age");
 
 setInterval(() => {
 	let time = (new Date() - new Date(1045593960000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
-	ageEl.innerText = time.toString().substring(0, 11);
+	ageEl.innerText = time.toString().substring(0, 10);
 }, 500);
 
 let nameEl = document.getElementById("name");
+let tName = ['','','_','A','A_','Aa','Aa_','Aar','Aar_','Aara','Aara_','Aarav','Aarav_'];
+let cName=['Aarav','Aarav_'];
 
-let alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let oName = '';
+//Make it look cooler by having my name display as being typed
+// Idea taken from otto's portfolio but code written by me :)
 
-//Make it look cooler by having my name display as randomised letters :)
-
-setInterval(() => {	
-	//nameEl.innerText = (Math.floor(Math.random()*26)+1).toString()
-	for (let i = 0; i < 5; i++) {
-		oName += alpha[(Math.floor(Math.random()*26))]
+let i = 0;
+function nextChar() {
+	nameEl.innerText = tName[i];
+	i++;
+	if (i < tName.length) {
+		setTimeout(nextChar,200);
+	} else {
+		i = 0;
+		setTimeout(() => {
+			setInterval(() => {
+				nameEl.innerText = cName[i]
+				i++
+				if (i>1) {
+					i=0;
+				}
+			}, 200);
+		}, 500);
 	}
-	oName = oName.slice(0,1).toUpperCase() + oName.slice(1,5)
-	if (oName=='Aarav') {
-	// This code alerts if the randomised letters become my actual name which is a 1 in 11881376 chance :)
-		window.alert('You have been luckily shown my real name. Screenshot and share this with me :)')
-	}
-	nameEl.innerText = oName
-	oName=''
-},100);
+}
+
+nextChar();
+
+//didn't expect otto's code would be similar to this but istg i wrote it on my own
+
