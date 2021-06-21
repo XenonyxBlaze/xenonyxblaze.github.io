061d05968572c170d30305e2665e24207ae89ad3
AaravRajput <aarav.rajput2003@gmail.com>
2021-06-21 22:05:28 +0530

    final site intro

--- js/info.js
+++ js/info.js
@@ -1,15 +1,18 @@
-// Age code taken from ottomated's portfolio. Hopefully he doesn't mind it much. He's only 10 days older than me :O
+// Age code inspired from ottomated's portfolio. Hopefully he doesn't mind it much. He's only 10 days older than me :O
+console.log("Hello World")
+console.log("Welcome to my site")
+console.log("Hello, did you find this easter egg ?? lemme know on discord ;)")
 
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
 	let time = (new Date() - new Date(1045593960000)) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
-	ageEl.innerText = time.toString().substring(0, 10);
-}, 500);
+	ageEl.innerText = time.toString().substring(0, 12);
+}, 50);
 
 let nameEl = document.getElementById("name");
-let tName = ['','','_','A','A_','Aa','Aa_','Aar','Aar_','Aara','Aara_','Aarav','Aarav_'];
-let cName=['Aarav','Aarav_'];
+let tName = ['','','_','A','A_','A','Aa_','Aa','Aa_','Aar_','Aar','Aar_','Aara_','Aara','Aara_','Aarav_','Aarav','Aarav_','Aarav _','Aarav ','Aarav _','Aarav R_','Aarav R','Aarav R_','Aarav Ra_','Aarav Ra','Aarav Ra_','Aarav Raj_','Aarav Raj','Aarav Raj_','Aarav Rajp_','Aarav Rajp','Aarav Rajp_','Aarav Rajpu_','Aarav Rajpu','Aarav Rajpu_','Aarav Rajput_','Aarav Rajput'];
+
 
 //Make it look cooler by having my name display as being typed
 // Idea taken from otto's portfolio but code written by me :)
@@ -19,22 +22,9 @@ function nextChar() {
 	nameEl.innerText = tName[i];
 	i++;
 	if (i < tName.length) {
-		setTimeout(nextChar,200);
-	} else {
-		i = 0;
-		setTimeout(() => {
-			setInterval(() => {
-				nameEl.innerText = cName[i]
-				i++
-				if (i>1) {
-					i=0;
-				}
-			}, 200);
-		}, 500);
+		setTimeout(nextChar,100);
 	}
 }
 
-nextChar();
 
 //didn't expect otto's code would be similar to this but istg i wrote it on my own
-
---
+++ js/start.js
@@ -0,0 +1,27 @@
+// Typewriter effect of writing console.log commands 
+// looks cool
+
+let mainEl = document.getElementById('main')
+
+let consoleEl = document.getElementById('console');
+
+let comm1 = "               >console.log(\"Hello World\");\n>console.log(\"Welcome to my site\");".split("");
+
+let x = 0;
+let comm2 = "";
+let start = () => {
+    
+    comm2 += comm1[x];
+    consoleEl.innerText = comm2;
+    x++
+    if (x<comm1.length) {
+        setTimeout(start,100)
+    } else {
+        setTimeout(() => {
+            consoleEl.classList.add('d-none');
+            mainEl.classList.remove('d-none');
+            nextChar();
+        }, 2000);
+    }
+}
+start();
